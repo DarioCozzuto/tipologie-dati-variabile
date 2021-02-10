ac818d38a117e792343d24c68ef53606a1e710b5
DarioCozzuto <77857380+DarioCozzuto@users.noreply.github.com>
2021-02-10 20:51:23 +0100

    Add files via upload

---
+++ tipologie dati variabile.php
@@ -0,0 +1,13 @@
+<?php
+$x = "Hello world!";   //stringa di testo
+
+$x = 5985;             //numero intero compreso tra -2,147,483,648 e 2,147,483,647
+var_dump($x);          //stampa il tipo di numero ed il numero di $x
+
+$x = 10.365;
+
+$x = true;             //booleano
+$y = false;            //booleano
+
+$cars = array("Volvo","BMW","Toyota");    //array
+?>
